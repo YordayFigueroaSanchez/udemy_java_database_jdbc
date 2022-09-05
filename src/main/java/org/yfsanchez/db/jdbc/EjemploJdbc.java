616534617548dd0a327fab05b31626262bbcdfb1
yfsanchez <yfsanchez@gmail.com>
2022-09-04 22:54:55 -0300

    Section 34 : 344-Implementacion de la clase Repository parte guardarUpdate -- Test

--- src/main/java/org/yfsanchez/db/jdbc/EjemploJdbc.java
+++ src/main/java/org/yfsanchez/db/jdbc/EjemploJdbc.java
@@ -6,6 +6,7 @@ import org.yfsanchez.db.jdbc.repository.RepositoryProductoImpl;
 import org.yfsanchez.db.jdbc.utils.ConectionDataBase;
 
 import java.sql.*;
+import java.util.Date;
 
 public class EjemploJdbc {
     public static void main(String[] args) {
@@ -18,6 +19,19 @@ public class EjemploJdbc {
             repositoryProduto.listar().forEach(p -> System.out.println(p.toString()));
 
             System.out.println(repositoryProduto.porId(1L));
+
+//            Producto producto = new Producto();
+//            producto.setNombre("Test03");
+//            producto.setPrecio(12);
+//            producto.setFecha(new Date());
+//            repositoryProduto.guardar(producto);
+
+//            Producto producto = new Producto();
+//            producto.setNombre("Test03");
+//            producto.setPrecio(1222);
+//            producto.setFecha(new Date());
+//            producto.setId(3L);
+//            repositoryProduto.guardar(producto);
 
         } catch (SQLException e) {
             throw new RuntimeException(e);
